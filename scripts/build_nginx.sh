04764fd7e85ac3f3e75b561f1181f7cac0c618db
Benjamin Murden <ben.murden@gmail.com>
2017-02-15 17:15:55 +0900

    Remove unnecessary dependencies. Fix compile error

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -6,17 +6,13 @@
 # Our motivation for running in a web dyno is that we need a way to
 # download the binary once it is built so we can vendor it in the buildpack.
 #
-# Once the dyno has is 'up' you can open your browser and navigate
+# Once the dyno is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
 
 NGINX_VERSION=${NGINX_VERSION-1.11.3}
-PCRE_VERSION=${PCRE_VERSION-8.39}
-ZLIB_VERSION=${ZLIB_VERSION-1.2.8}
 NPS_VERSION=${NPS_VERSION-1.12.34.2}
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
-pcre_tarball_url=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${PCRE_VERSION}.tar.gz
-zlib_url=http://zlib.net/zlib-${ZLIB_VERSION}.tar.gz
 nps_url=https://github.com/pagespeed/ngx_pagespeed/archive/v${NPS_VERSION}-beta.tar.gz
 
 temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
@@ -29,32 +25,28 @@ cd $temp_dir
 echo "Temp dir: $temp_dir"
 
 echo "Downloading $nginx_tarball_url"
-curl -L $nginx_tarball_url | tar xzv
-
-echo "Downloading $pcre_tarball_url"
-(cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xvz )
-
-echo "Downloading $zlib_url"
-(cd nginx-${NGINX_VERSION} && curl -L $zlib_url | tar xvz )
+curl -L $nginx_tarball_url | tar xz
 
 echo "Downloading $nps_url"
 ( 
-  cd nginx-${NGINX_VERSION} && curl -L $nps_url | tar xvz
-  cd ngx_pagespeed-release-${NPS_VERSION}-beta/
-  wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
-  tar -xzvf ${NPS_VERSION}.tar.gz
+  cd nginx-${NGINX_VERSION} && curl -L $nps_url | tar xz
+  cd ngx_pagespeed-${NPS_VERSION}-beta/
+  psol_url=https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
+  [ -e scripts/format_binary_url.sh ] && psol_url=$(scripts/format_binary_url.sh PSOL_BINARY_URL)
+  echo "Downloading $psol_url"
+  wget ${psol_url}
+  tar -xzf $(basename ${psol_url})
 )
 
 (
   cd nginx-${NGINX_VERSION}
   ./configure \
-    --with-pcre=pcre-${PCRE_VERSION} \
-    --with-zlib=zlib-${ZLIB_VERSION} \
-    --add-module=nginx-${NGINX_VERSION}/ngx_pagespeed-${NPS_VERSION}-beta
+    --add-module=${temp_dir}/nginx-${NGINX_VERSION}/ngx_pagespeed-${NPS_VERSION}-beta
     --prefix=/tmp/nginx \
     --with-http_gzip_static_module \
     --with-cc-opt='-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2' \
-    --with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,--as-needed'
+    --with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,--as-needed' \
+    ${PS_NGX_EXTRA_FLAGS}
 
   make install
 )
