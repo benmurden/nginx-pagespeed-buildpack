80e28b07f776ee991b52b947a02ee245ba98c2a0
Benjamin Murden <ben.murden@gmail.com>
2017-02-15 23:22:33 +0900

    Build new binary for cedar-14

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -45,8 +45,7 @@ echo "Downloading $nps_url"
     --prefix=/tmp/nginx \
     --with-http_gzip_static_module \
     --with-cc-opt='-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2' \
-    --with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,--as-needed' \
-    ${PS_NGX_EXTRA_FLAGS}
+    --with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,--as-needed' 
 
   make install
 )
