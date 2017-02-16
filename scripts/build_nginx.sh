5a55e6fb02a276c58593d65c4d120fb6ad9b35f2
Benjamin Murden <ben.murden@gmail.com>
2017-02-16 16:49:00 +0900

    Update nginx version

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -9,7 +9,7 @@
 # Once the dyno is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
 
-NGINX_VERSION=${NGINX_VERSION-1.11.3}
+NGINX_VERSION=${NGINX_VERSION-1.11.10}
 NPS_VERSION=${NPS_VERSION-1.12.34.2}
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
