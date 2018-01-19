d89370df3d6b92919cbe761b03d0b595d7bbb096
Benjamin Murden <ben.murden@gmail.com>
2018-01-19 14:29:31 +0900

    Fix module directory

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -42,7 +42,7 @@ tar -xzf $(basename ${psol_url})
 cd ${temp_dir}/nginx-${NGINX_VERSION}
 ./configure \
   --prefix=/tmp/nginx \
-  --add-module=${temp_dir}/nginx-${NGINX_VERSION}/${nps_dir} \
+  --add-module=${temp_dir}/nginx-${NGINX_VERSION}/${nps_dir}/ \
   --with-http_gzip_static_module \
   --with-cc-opt='-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2' \
   --with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,--as-needed' 
